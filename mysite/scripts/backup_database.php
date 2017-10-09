77c6e8dba7b64cec7663464dffc79667d3cbb1c2
Jake B <jake@symbiote.com.au>
2017-10-09 13:35:45 +1100

    FIX, Add support for 3.x naming of the classes as the backup system pulls this logic from `master` branch currently

--- mysite/scripts/backup_database.php
+++ mysite/scripts/backup_database.php
@@ -13,11 +13,14 @@ $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 chdir(dirname(dirname(dirname(__FILE__))).'/framework');
 require_once 'core/Core.php';
 
-$outfile = isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : $outfile = Director::baseFolder().'/mysite/scripts/backup-'.date('Y-m-d-H-i-s').'.sql.gz';
+// Support Silverstripe 4.x or Silverstripe 3.x
+$baseFolder = class_exists(Director::class) ? Director::baseFolder() : \Director::baseFolder();
+$outfile = isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : $outfile = $baseFolder.'/mysite/scripts/backup-'.date('Y-m-d-H-i-s').'.sql.gz';
 
 global $databaseConfig;
 
 switch ($databaseConfig['type']) {
+	case 'MySQLDatabase': // Backwards compat, Silverstripe 3.x support
 	case MySQLDatabase::class:
 		$u = $databaseConfig['username'];
 		$p = $databaseConfig['password'];
@@ -27,7 +30,7 @@ switch ($databaseConfig['type']) {
 		$cmd = "mysqldump --user=".escapeshellarg($u)." --password=".escapeshellarg($p)." --ignore-table=$d.details --host=".escapeshellarg($h)." ".escapeshellarg($d)." | gzip > ".escapeshellarg($outfile);
 		exec($cmd);
 		break;
-	case 'SQLiteDatabase':
+	case 'SQLiteDatabase': // Backwards compat, Silverstripe 3.x support
 	case SQLite3Database::class:
 		$d = $databaseConfig['database'];
 		$path = realpath(dirname(__FILE__).'/../../assets/.sqlitedb/'.$d);
